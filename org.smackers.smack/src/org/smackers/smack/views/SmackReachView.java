d25aa7cffc95a8aefc698985a1cbac32279f5354
snoopsmsc <montgomery.carter@gmail.com>
2014-08-08 14:49:15 -0600

    CLOSED #15: Deactivate markers in editor when tabular view not active

--- org.smackers.smack/src/org/smackers/smack/views/SmackReachView.java
+++ org.smackers.smack/src/org/smackers/smack/views/SmackReachView.java
@@ -4,7 +4,10 @@ package org.smackers.smack.views;
 public class SmackReachView extends SmackView {
 
 	public SmackReachView() {
-		super("org.smackers.smack.markers.SmackdReachMarkerGenerator");
+		super("org.smackers.smack.markers.SmackdReachMarkerGenerator",
+				new String[]{
+					"org.smackers.smack.markers.smackUnreachableAnnotation"
+				});
 	}
 	
 }
